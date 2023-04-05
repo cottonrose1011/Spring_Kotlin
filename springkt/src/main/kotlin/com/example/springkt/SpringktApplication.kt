dc780255dc5c26b136b0b2c4f2040fa4b0e4b9b9
cottonrose1011 <pjeka7er43@gmail.com>
2023-04-05 15:51:21 +0900

    0405

--- springkt/src/main/kotlin/com/example/springkt/SpringktApplication.kt
+++ springkt/src/main/kotlin/com/example/springkt/SpringktApplication.kt
@@ -3,7 +3,9 @@ package com.example.springkt
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
@@ -13,14 +15,21 @@ fun main(args: Array<String>) {
 	runApplication<SpringktApplication>(*args)
 }
 
-var counter = 1
+var news = mutableListOf("2030부산세계박람회 유치기원 ‘불꽃쇼’ 안전 총력 대응! ", "커터칼로 초등생 목 그은 고등학생 긴급체포", "광화문광장서 신원 미상 중년 남성 분신...중태 빠져")
+
 
 @Controller
 class MainController {
-
 	@GetMapping("/")
-	@ResponseBody
-	fun index(): String {
-		return "${counter++} 번째 접속자"
+	fun home(model: Model): String {
+		model.addAttribute("newsList", news)
+		return "news"
 	}
+
+	@PostMapping("/")
+	fun add(txt: String): String {
+		news.add(txt)
+		return "redirect:/"
+	}
+
 }
