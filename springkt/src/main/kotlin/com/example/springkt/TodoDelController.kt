9b50d540b1b775b7eff1980b7c758373f47e830c
cottonrose1011 <pjeka7er43@gmail.com>
2023-06-07 01:40:24 +0900

    2단계완료

--- springkt/src/main/kotlin/com/example/springkt/TodoDelController.kt
+++ springkt/src/main/kotlin/com/example/springkt/TodoDelController.kt
@@ -2,8 +2,7 @@ package com.example.springkt
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.*
 
 var count : Int = 0
 data class Todo(val id: Int, val title: String)
@@ -29,5 +28,11 @@ class TodoDelController {
         return "redirect:/todo"
     }
 
-
+    @DeleteMapping("/todo")
+    @ResponseBody
+    fun deleteTodo(@RequestBody delList: List<Int>) {
+        Result.runCatching { for (ids in delList){ todoList.removeAt(ids) } }
+        count--; //todoList의 index number와 일치 시키기 위함, 만약 count--를 하지 않고 3개중 하나를 삭제한 다음,
+                //하나를 다시 추가하면 그 리스트의 id는 3인데 현재 todoList는 0,1,2 까지만 있어 Index 오류가 발생하여 삭제를 하지않음.
+    }
 }
